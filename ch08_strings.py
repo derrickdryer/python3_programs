c02a5a58aaf1ea5e248106611d0c9cc3a44da757
Derrick Dryer <derrickdryer@proton.me>
2024-02-27 15:50:28 -0500

    Learned about manipuating strings.

---
+++ ch08_strings.py
@@ -0,0 +1,35 @@
+userDate = input("Enter a date (mm/dd/yyyy): ")
+
+monthName = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+
+
+def extractDate(userDate):
+    month = userDate[0:2]
+    day = userDate[3:5]
+    year = userDate[6:10]
+    return month, day, year
+
+
+def main():
+    month, day, year = extractDate(userDate)
+    month = int(month)
+    month -= 1
+    day = int(day)
+    year = int(year)
+    print("The date is", monthName[month], str(day) + ",", year)
+
+
+main()
